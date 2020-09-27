4465e1616eb8b3759254e7eb55b0de8aec2e35a5
ayoub <ayoub.belghar@gmail.com>
2020-09-28 00:20:02 +0200

    add name

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From scratch
+          Hi Ayoub
         </a>
       </header>
     </div>
